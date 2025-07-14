717108de40a9061afebb1b51e4dc3b3fc6d1c785
Yuyang Deng <dyyjason1212@163.com>
2025-07-14 17:15:37 +0800

    Add- TBSS epinette dataset cleanup pipeline

--- TBSS_pipeline/cleanup_EPINETTE_DWI.sh
+++ TBSS_pipeline/cleanup_EPINETTE_DWI.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-for file in GENCORE*_T1*.nii.gz; do
-  # Extract parts using regex
-  num1=$(echo "$file" | sed -E 's/GENCORE([0-9]+)_T1.*/\1/')
-  num2=$(echo "$file" | sed -E 's/.*_T1(_GD)?_([0-9]{8})_.*/\2/')
-  remainder=$(echo "$file" | sed -E 's/.*_T1(_GD)?_[0-9]{8}_(.*)\.nii\.gz/\2/')
+# Define the base directory
+base_dir="/work/users/d/y/dyy12/WORK_TBSS/DWI"
 
-  # Compose folder name
-  folder="${num1}_${num2}_${remainder}"
-  mkdir -p "$folder"
+# Make sure we are in the current working directory where the DWI files are located
+cd /work/users/d/y/dyy12/WORK_TBSS/DWI || exit
 
-  # Move and rename the file
-  mv "$file" "$folder/T1.nii.gz"
+# Loop over all .nii.gz files
+for nii_file in *.nii.gz; do
+    # Strip the .nii.gz to get the base name
+    base_name="${nii_file%.nii.gz}"
+
+    # Create a directory with the base name under the destination path
+    target_folder="${base_name}"
+    mkdir -p "$target_folder"
+
+    # Move the .nii.gz, .bval, and .bvec files into the new folder if they exist
+    [ -f "${base_name}.nii.gz" ] && mv "${base_name}.nii.gz" "$target_folder/"
+    [ -f "${base_name}.bval" ] && mv "${base_name}.bval" "$target_folder/"
+    [ -f "${base_name}.bvec" ] && mv "${base_name}.bvec" "$target_folder/"
 done
