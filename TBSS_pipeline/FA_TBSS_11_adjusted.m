444735918506a2fc7683a28c4db82c42ac9dbd93
Yuyang Deng <dyyjason1212@163.com>
2025-08-20 22:50:16 -0400

    Update- TBSS pipeline python version bug

--- TBSS_pipeline/FA_TBSS_11_adjusted.m
+++ TBSS_pipeline/FA_TBSS_11_adjusted.m
@@ -1,3 +1,5 @@
 module purge # Clear all loaded modules
 module load fsl/5.0.9 # Load FSL module
 module load python/2.7.12 # Load Python module
+
+# Instead, use previous version of the Longleaf cluster to complte the task.
